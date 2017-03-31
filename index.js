25cc0cf159022aad656ab328483afb0129eac65c
ahazardkampmann <kampmann.alexandra@bcg.com>
2017-03-31 19:13:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,10 @@ app.get('/', function(request, response) {
   response.send('Hello Alex!')
 })
 
+app.get('/what', function(request, response) {
+  response.send('What what!')
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
